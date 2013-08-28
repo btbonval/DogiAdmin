05798d265c9892e1df44f8e1d6267118f00a583c
Bryan Bonvallet <btbonval@gmail.com>
2013-08-28 01:11:06 -0400

    WIP moving closer to a tac-style twistd-ready file

--- server/main.py
+++ server/main.py
@@ -7,6 +7,12 @@ and updated at runtime.
 '''
 
 ##
+# External modules
+##
+
+from twisted.internet import endpoints
+
+##
 # Built-in modules
 ##
 
@@ -22,3 +28,14 @@ if __name__ == '__main__':
     daf = DogiAdminFactory(config)
     endpoints.serverFromString(reactor, daf.gen_server_string_from_config()).listen(daf)
     reactor.run()
+
+##
+# if run by twistd or as a module
+##
+
+else:
+    application = service.Application('dogiadminserver')
+    daf = DogiAdminFactory(config)
+    # TODO not sure how this works with twistd?
+    # https://twistedmatrix.com/documents/current/core/howto/endpoints.html
+    endpoints.serverFromString(reactor, daf.gen_server_string_from_config()).listen(daf)
