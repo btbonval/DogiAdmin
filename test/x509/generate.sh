7284d3d6eccbadb6c6edde6372712d04a846fe1a
Bryan <btbonval@gmail.com>
2013-09-25 22:48:34 -0400

    TSL works perfectly with v3 x509 certificates, proper fix for #12

--- test/x509/generate.sh
+++ test/x509/generate.sh
@@ -10,24 +10,24 @@ cd $CERTDIR
 
 # create two Certificate Authorities
 NAME=CA1
-openssl req -config $CACFG -new -x509 -keyout ${NAME}.ca.rsa -out ${NAME}.ca.crt -days 1825
+openssl req -config $CACFG -new -x509 -keyout ${NAME}.rsa -out ${NAME}.crt -days 1825
 NAME=CA2
-openssl req -config $CACFG -new -x509 -keyout ${NAME}.ca.rsa -out ${NAME}.ca.crt -days 1825
+openssl req -config $CACFG -new -x509 -keyout ${NAME}.rsa -out ${NAME}.crt -days 1825
 
-# create three Certificates
+# create three Certificates with signature requests
 NAME=CERT1A
-openssl req -config $CERTCFG -new -keyout ${NAME}.rsa -out ${NAME}.req
+openssl req -config $CERTCFG -new -keyout ${NAME}.rsa -out ${NAME}.csr
 NAME=CERT1B
-openssl req -config $CERTCFG -new -keyout ${NAME}.rsa -out ${NAME}.req
+openssl req -config $CERTCFG -new -keyout ${NAME}.rsa -out ${NAME}.csr
 NAME=CERT2
-openssl req -config $CERTCFG -new -keyout ${NAME}.rsa -out ${NAME}.req
+openssl req -config $CERTCFG -new -keyout ${NAME}.rsa -out ${NAME}.csr
 
 # sign three Certificates by CAs
 NAME=CERT1A
 CA=CA1
-openssl x509 -req -in ${NAME}.req -CA ${CA}.ca.crt -CAkey ${CA}.ca.rsa -CAcreateserial -out ${NAME}.crt
+openssl x509 -req -extfile ../v3ext.cfg -extensions v3e -in ${NAME}.csr -CA ${CA}.crt -CAkey ${CA}.rsa -CAcreateserial -out ${NAME}.crt
 NAME=CERT1B
-openssl x509 -req -in ${NAME}.req -CA ${CA}.ca.crt -CAkey ${CA}.ca.rsa -CAcreateserial -out ${NAME}.crt
+openssl x509 -req -extfile ../v3ext.cfg -extensions v3e -in ${NAME}.csr -CA ${CA}.crt -CAkey ${CA}.rsa -CAcreateserial -out ${NAME}.crt
 NAME=CERT2
 CA=CA2
-openssl x509 -req -in ${NAME}.req -CA ${CA}.ca.crt -CAkey ${CA}.ca.rsa -CAcreateserial -out ${NAME}.crt
+openssl x509 -req -extfile ../v3ext.cfg -extensions v3e -in ${NAME}.csr -CA ${CA}.crt -CAkey ${CA}.rsa -CAcreateserial -out ${NAME}.crt
