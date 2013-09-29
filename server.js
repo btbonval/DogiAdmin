d48380401453d319a190dd8b4d145c72db961450
Bryan <btbonval@gmail.com>
2013-09-29 17:04:32 -0400

    track clients by identifier for assisting #10

--- server.js
+++ server.js
@@ -36,6 +36,9 @@ function run_server(cfg) {
     var prot = { true: tls, false: net };
     var opts = { true: sslopts.filter_options(cfg.ssl), false: {} }
 
+    // listen for clients
+    // track connected clients
+    var clientlist = new Object();
     // call the correct server listener given the configuration.
     var server = prot[pcl].createServer(opts[pcl], function(c) {
         // do not accept unauthorized connections if using TSL
@@ -44,12 +47,10 @@ function run_server(cfg) {
             socket.end();
             return;
         }
-        if (pcl) {
-            var id = c.getPeerCertificate()
-            console.log('Connection from', id.fingerprint);
-        } else {
-            console.log('Connection!');
-        }
+        // generate client identifier and store it to the clientlist
+        var id = client_id(clientlist, c);
+        console.log('Connection from ' + id.toString());
+
         process.stdin.resume();
     
         // pipe my terminal IO through the socket's IO
@@ -61,13 +62,50 @@ function run_server(cfg) {
             c.unpipe(process.stdout);
     
             c.end();
+
+            // stop tracking client
+            delete clientlist[clientlist[c]];
+            delete clientlist[c];
     
             process.stdin.pause();
-            console.log('Connection closed.');
+            console.log('Connection closed from ' + id.toString());
         });
     })
     server.maxConnections = 1;
     server.listen(cfg.server.port, cfg.server.address, function() {
         console.log('Listening.');
     });
+}
+
+// how to identify a client
+function client_id(clientlist, socket) {
+    // given the clientlist and socket, determine the socket's identifier and
+    // associate it within the clientlist.
+    // return the client identifier for good measure.
+
+    //var iscert = socket instanceof tls.CleartextStream;
+    var iscert = socket.hasOwnProperty('authorized')
+
+    var id;
+    if (iscert) {
+        // TLS client identification
+        // for now, it's just the certificate fingerprint
+        id = socket.getPeerCertificate().fingerprint;
+    } else {
+        // plain text client identification
+        // initialize counter if needed
+        if (! clientlist.hasOwnProperty('counter') ) {
+            clientlist['counter'] = 0;
+        }
+
+        // assign anonymous counter numbers for guest logins
+        id = clientlist['counter'];
+
+        // increment counter for the next login
+        clientlist['counter'] = clientlist['counter'] + 1;
+    }
+    // cache the associated identification with the connection.
+    clientlist[id] = socket;
+    clientlist[socket] = id;
+    return id;
 }
